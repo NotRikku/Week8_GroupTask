ca3f31cfcaac1deb26fa2733f34175069de528f5
NotRikku <cedric.eltagonde@gmail.com>
2026-07-02 03:03:44 +0800

    Changes

---
+++ Week8_GroupTask/app/(tabs)/index.tsx
@@ -0,0 +1,84 @@
+import {
+  View,
+  Text,
+  FlatList,
+  StyleSheet,
+  Button,
+} from 'react-native';
+
+const menuItems = [
+  { id: '1', category: 'Hot Drinks', name: 'Americano' },
+  { id: '2', category: 'Hot Drinks', name: 'Cappuccino' },
+  { id: '3', category: 'Hot Drinks', name: 'Latte' },
+
+  { id: '4', category: 'Cold Drinks', name: 'Iced Coffee' },
+  { id: '5', category: 'Cold Drinks', name: 'Frappuccino' },
+
+  { id: '6', category: 'Desserts', name: 'Cheesecake' },
+  { id: '7', category: 'Desserts', name: 'Brownie' },
+  { id: '8', category: 'Desserts', name: 'Chocolate Cake' },
+];
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.heading}>Placeholder Cafe</Text>
+      <Text style={styles.subtitle}>Coffee Shop Menu</Text>
+
+      <FlatList
+        data={menuItems}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <View style={styles.item}>
+            <Text style={styles.category}>{item.category}</Text>
+
+            <Text style={styles.name}>{item.name}</Text>
+
+            <Button
+             title="View Item"
+             onPress={() =>
+              console.log(
+                `Category: ${item.category}, Item: ${item.name}`
+                )
+              }
+            />
+          </View>
+        )}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+  heading: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    textAlign: 'left',
+    marginBottom: 20,
+  },
+  subtitle: {
+  textAlign: 'center',
+  fontSize: 16,
+  color: 'gray',
+  marginBottom: 20,
+  },
+  item: {
+    padding: 12,
+    marginBottom: 15,
+    borderWidth: 1,
+    borderColor: '#ddd',
+    borderRadius: 8,
+  },
+  category: {
+    fontSize: 12,
+    color: 'gray',
+  },
+  name: {
+    fontSize: 18,
+    marginBottom: 8,
+  },
+});
